46137ad51013b07f18e53b221dfcf9fb2b6ec57f
ryansohny <phenomata@gmail.com>
2024-10-07 21:55:46 -0700

    modified some

--- _pick_variant_type.sh
+++ _pick_variant_type.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=cpu-g2
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
-#SBATCH --time=15:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mem=5G
 #SBATCH -o logs/slurm.%N.%j.out
 #SBATCH -e logs/slurm.%N.%j.err
@@ -15,15 +15,11 @@
 #SBATCH --mail-user=mhsohny@uw.edu
 #SBATCH --export=ALL
 
-source activate base
-conda activate mcg
-
 dir=/mmfs1/gscratch/stergachislab/mhsohny/SMaHT/Improving_SomaticVariantCalling_through_DSA/Fiber-seq/VariantCalls_DeepVariant_1.6.1
 
 for sample in COLO829BL COLO829T_PassageA_DSA COLO829T_PassageB_DSA
 do
 
-:<<'END'
 echo "Split Multialleles for ${sample}"
 bcftools norm \
 --write-index=tbi \
@@ -37,7 +33,6 @@ bcftools view \
 --write-index=tbi \
 ${dir}/${sample}/deepvariant/${sample}.deepvariant.split.vcf.gz \
 -o ${dir}/${sample}/deepvariant/${sample}.deepvariant.split.snv.vcf.gz
-END
 
 zcat ${dir}/${sample}/deepvariant/${sample}.deepvariant.split.snv.vcf.gz | \
 awk 'BEGIN {OFS="\t"} { if (/^#/) { print } else { $4 = substr($4, 1, 1); $5 = substr($5, 1, 1); print } }' | \
@@ -47,5 +42,20 @@ tabix -p vcf ${dir}/${sample}/deepvariant/${sample}.deepvariant.split.snv.modifi
 
 #Check out the things below (check to see if these are present. If they are there, something went wrong):
 #zcat ${sample}.deepvariant.split.snv.vcf.gz | awk '!/^#/ && length($4)>=2 && substr($4, 2) != substr($5, 2)' - | less -S
+
+bedtools intersect \
+-wao \
+-a ${dir}/${sample}/deepvariant/${sample}.deepvariant.split.snv.modified.vcf.gz \
+-b /mmfs1/gscratch/stergachislab/mhsohny/SMaHT/DSA/Flagger_v0.3.3/DSA_COLO829BL_v3.0.0.alt_removed.flagger_final.bed | \
+awk '{print $1 "_" $2  "_" $4 "_" $5 "\t" $7 "\t" $(NF-6)}' > \
+${dir}/${sample}/deepvariant/${sample}.deepvariant.split.snv.modified.flagger
+
+bedtools intersect \
+-wao \
+-a ${dir}/${sample}/deepvariant/${sample}.deepvariant.split.snv.modified.vcf.gz \
+-b /mmfs1/gscratch/stergachislab/mhsohny/SMaHT/DSA/NucFreq_v0.1/DSA_COLO829BL_v3.0.0.nucfreq.final_qc_sorted.bed | \
+rg -ve $'-1\t-1\t.' | \
+awk '{print $1 "_" $2  "_" $4 "_" $5 "\t" $7 "\t" $(NF-1)}' > \
+${dir}/${sample}/deepvariant/${sample}.deepvariant.split.snv.modified.nucfreq
 
 done
